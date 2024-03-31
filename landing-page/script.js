6c5289ff442bbeecd3797843ecd986beff4d7412
Karim JENZRI <kjenzri@yahoo.com>
2024-03-31 12:38:18 +0200

    mobile design in progress

--- landing-page/script.js
+++ landing-page/script.js
@@ -10,4 +10,28 @@ function validateForm(e) {
       e.stopImmediatePropagation();
       formErrorMessage.style.visibility = "visible";
     }
-  };
+};
+
+function openMenu(){
+  const data_stateConst = "data-state";
+  const closeConst = "close";
+  const openConst = "open";
+  const menuElementSelectors = Array(".menu", ".menu ul", ".menu > ul > li > a", "body");
+  const stateIcon = new Map([
+    [openConst, "images/icon-close.svg"],
+    [closeConst, "images/icon-hamburger.svg"]
+  ]);
+
+  const menuImage = document.querySelector('.hamburger-menu');
+  const newState = menuImage.attributes[data_stateConst].value === closeConst ? openConst : closeConst;
+
+  menuElementSelectors.forEach(selector => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+      element.setAttribute(data_stateConst, newState);  
+    });
+  });
+
+  menuImage.src = stateIcon.get(newState);
+  menuImage.attributes[data_stateConst].value = newState;
+}
