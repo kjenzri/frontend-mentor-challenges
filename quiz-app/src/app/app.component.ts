81db59daad7a6b2c9c1a509ef2da3586ca9bc483
Karim JENZRI <kjenzri@yahoo.com>
2024-10-12 18:42:33 +0200

    fix dark light mode switch and links color

--- quiz-app/src/app/app.component.ts
+++ quiz-app/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [ RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive,
+    FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'quiz-app';
+
+export class AppComponent implements OnInit {
+  private readonly trueLiteral = 'true';
+  private readonly darkModeKey = 'lightMode';
+  darkMode = false;
+
+  ngOnInit(): void {
+    this.darkMode = localStorage.getItem(this.darkModeKey) === this.trueLiteral;
+  }
+  
+  onLightDarkChange(){
+    localStorage.setItem(this.darkModeKey, String(this.darkMode));
+  }
 }
