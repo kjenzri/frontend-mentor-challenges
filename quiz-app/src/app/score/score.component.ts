ea45ccf024ad98c0ceac290901ebd29f53c78c8a
Karim JENZRI <kjenzri@yahoo.com>
2024-11-07 07:52:13 +0100

    finalize score component

--- quiz-app/src/app/score/score.component.ts
+++ quiz-app/src/app/score/score.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-score',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './score.component.html',
   styleUrl: './score.component.css'
 })
 export class ScoreComponent {
-
 }
